0b8042bd036ed2499d06bf2e6341572bf7493ffd
ho87nyze <kolo93tosO>
2022-11-11 13:56:05 +0100

    Initial commit

---
+++ src/cmd_parser.py
@@ -0,0 +1,23 @@
+# coding=utf-8
+"""Contains methods for command line interaction"""
+import argparse
+
+
+def parse_args():
+    """
+    Creates the argument parser for the command line
+    :return: the args that were parsed from the command line
+    """
+    # This file contains the commandline tools for the current tool
+    parser = argparse.ArgumentParser(prog='psgrade.py')
+    parser.add_argument('--inputcsv', nargs='*', help='The input csv', required=True)
+    parser.add_argument('--gradetable', help='The grade table to ba amended', required=True)
+    parser.add_argument('--plag', default=False, action=argparse.BooleanOptionalAction)
+    parser.add_argument('--distribution_code', nargs='*')
+    parser.add_argument('--psetId', type=str, required=True)
+    parser.add_argument('--tasks', nargs='*', type=str.lower)
+    parser.add_argument('--choices', nargs='*', type=str.lower, help="e.g.: task1-task2 task3-task4-task5")
+    parser.add_argument("--archive",  default=False, action=argparse.BooleanOptionalAction)
+    parser.add_argument("--sentimental",  default=False, action=argparse.BooleanOptionalAction)
+
+    return parser.parse_args()
