1e1a71ed918f18784406c946bf250948d0040195
Sebastian Dunzer <sebastian.dunzer@gmail.com>
2022-11-15 09:55:42 +0100

    Rewrote IntroCSGrading. Adjusted structure.

---
+++ psgrade/Results/results.py
@@ -0,0 +1,111 @@
+from csv import DictReader
+
+
+class ResultsGenerator:
+    def __init__(self):
+        self.__submit50CsvDicts = []
+        self.__studentResultsExtracted = dict()
+        self.__passingStudents = []
+        self.__studentResultsFinal = dict()
+        self.__plagiarisingStudents = None
+
+    @property
+    def PlagiarisingStudents(self):
+        return self.__plagiarisingStudents
+
+    @property
+    def PassingStudents(self):
+        return self.__passingStudents
+
+    @property
+    def CS50FilesAsDict(self):
+        return self.__submit50CsvDicts
+
+    @property
+    def StudentResultsPreliminary(self):
+        return self.__studentResultsExtracted
+
+    @property
+    def StudentResultsFinal(self):
+        return self.__studentResultsFinal
+
+    def load_reformat_CS50_dicts(self, cs50CsvList) -> None:
+        for file in cs50CsvList:
+            self.__read_CS50csv_into_dict(file)
+
+    def __read_CS50csv_into_dict(self, path) -> None:
+        with open(path) as f:
+            reader = DictReader(f)
+            submission_dict = dict()
+            for row in reader:
+                submission_dict[row["github_username"]] = row
+            self.__submit50CsvDicts.append(submission_dict)
+
+    def get_student_results(self, slugs, tasks, choices):
+        self.__calculate_individual_task_results(slugs)
+        self.__cumulate_tasks_into_full_results(tasks, choices)
+
+    def __calculate_individual_task_results(self, slugs):
+        for dictionary in self.CS50FilesAsDict:
+            for student in dictionary:
+
+                student_dict = dictionary[student]
+
+                if student_dict["style50_score"] == '' or student_dict["style50_score"] == "ERROR":
+                    style_score = 1
+                else:
+                    style_score = float(student_dict["style50_score"])
+
+                if (float(student_dict["checks_passed"]) / float(student_dict["checks_run"])) < 0.8 \
+                        or style_score < 0.8:
+                    continue
+                else:
+                    if student in self.__studentResultsExtracted:
+                        self.__studentResultsExtracted[student]["tasks"] += 1
+                        self.__studentResultsExtracted[student][student_dict["slug"]] = student_dict["github_url"]
+                        if "tasknames" in self.__studentResultsExtracted[student]:
+                            self.__studentResultsExtracted[student]["tasknames"].append(student_dict["slug"].split('/')[-1].lower())
+                        else:
+                            self.__studentResultsExtracted[student]["tasknames"] = [student_dict["slug"].split('/')[-1].lower()]
+                    else:
+                        self.__studentResultsExtracted[student] = {"tasks": 1, student_dict["slug"]: student_dict["github_url"],
+                                            "tasknames": [student_dict["slug"].split('/')[-1].lower()]}
+                    if student_dict["slug"] not in slugs:
+                        slugs.append(student_dict["slug"])
+
+    def __cumulate_tasks_into_full_results(self, tasks, choices):
+        for student in self.__studentResultsExtracted:
+            if "tasknames" in self.__studentResultsExtracted[student]:
+                if set(self.__studentResultsExtracted[student]["tasknames"]) >= set(tasks):
+                    self.__passingStudents.append(student)
+                elif len(choices) > 0:
+                    choice_fulfilled = False
+                    for choice in choices:
+                        tmp = False
+                        for task in choice:
+                            if task in self.__studentResultsExtracted[student]["tasknames"]:
+                                tmp = True
+                                break
+                        if not tmp:
+                            choice_fulfilled = False
+                            break
+                        else:
+                            choice_fulfilled = True
+                else:
+                    choice_fulfilled = True
+
+                if choice_fulfilled:
+                    self.__passingStudents.append(student)
+
+    def update_student_results_plagiarism(self, result_dict_after_plagiarism):
+        self.__studentResultsFinal = result_dict_after_plagiarism
+        for student in self.__studentResultsFinal:
+            if "IsPlag" in self.__studentResultsFinal[student] and self.__studentResultsFinal[student]["IsPlag"]:
+                if student in self.__passingStudents:
+                    self.__passingStudents.remove(student)
+                self.__plagiarisingStudents = [student] if self.__plagiarisingStudents is None else None
+                self.__plagiarisingStudents.append(student)
+
+    def update_results_no_plagiarism(self):
+        self.__studentResultsFinal = self.__studentResultsExtracted
+
---
+++ psgrade/psgrade.py
@@ -0,0 +1,145 @@
+from csv import DictReader, DictWriter
+
+from Input.cmd_parser import parse_args
+from Results.results import ResultsGenerator
+from Plagiarism.downloader import Downloader
+from Plagiarism.plagiarism import Plagiarism
+
+
+class Controller(object):
+
+    def __init__(self):
+        self.__slugs: list = []
+        self.__resultPaths: list = []
+        self.__plagiarismCheck: bool = False
+        self.__sentimental: bool = False
+        self.__archive: bool = False
+        self.__inputFilesSubmit50: list = []
+        self.__distributionCodeURL: list = []
+        self.__gradeTable: str = ""
+        self.__psetName = ""
+        self.__tasks = []
+        self.__choices = []
+
+    @property
+    def PlagiarismCheck(self) -> bool:
+        return self.__plagiarismCheck
+
+    @property
+    def Choices(self) -> list:
+        return self.__choices
+
+    @property
+    def Tasks(self) -> list:
+        return self.__tasks
+
+    @property
+    def ProblemSetName(self) -> str:
+        return self.__psetName
+
+    @property
+    def DistributionURL(self):
+        return self.__distributionCodeURL
+
+    @property
+    def GradeTable(self) -> str or None:
+        return self.__gradeTable
+
+    @property
+    def InputCS50Csv(self) -> [] or None:
+        return self.__inputFilesSubmit50
+
+    @property
+    def Slugs(self) -> [] or None:
+        return self.__slugs
+
+    @property
+    def ResultsPaths(self) -> [] or None:
+        return self.__resultPaths
+
+    @property
+    def Sentimental(self) -> bool:
+        return self.__sentimental
+
+    @property
+    def Archive(self) -> bool:
+        return self.__archive
+
+    def set_commandline_args(self):
+        args = parse_args()
+        self.__inputFilesSubmit50 = args.inputcsv
+        self.__gradeTable = args.gradetable
+        self.__distributionCodeURL = args.distribution_code
+        self.__psetName = f"Pset{args.psetId}"
+        self.__tasks = args.tasks
+        self.__archive = args.archive
+        self.__sentimental = args.sentimental
+        self.__choices = []
+        self.__plagiarismCheck = args.plag
+        if args.choices:
+            self.__choices = [choice.split("-") for choice in args.choices]
+
+    def write_grade_table(self, results, passing_students, plagiarism=None):
+        with open(self.__gradeTable) as fi:
+            with open(''.join(self.__gradeTable.split('.')[:-1]) + "_" + self.__psetName + ".csv", 'w') as fo:
+                reader = DictReader(fi)
+                fieldnames = reader.fieldnames
+                if self.__psetName not in fieldnames:
+                    fieldnames = fieldnames + [self.__psetName]
+                writer = DictWriter(fo, fieldnames)
+                writer.writeheader()
+
+                for row in reader:
+                    if self.__psetName in row and row[self.__psetName] not in {'0', '1'}:
+                        writer.writerow(row)
+                        continue
+                    elif row["Git_username"] in passing_students:
+                        row[self.__psetName] = 1
+                    elif plagiarism is not None and row["Git_username"] in plagiarism:
+                        row[self.__psetName] = "Similarity_" + str(results[row["Git_username"]]["PlagConfidence"])
+                    else:
+                        row[self.__psetName] = 0
+
+                    writer.writerow(row)
+
+
+def main():
+    controller = Controller()
+    controller.set_commandline_args()
+
+    results_generator = ResultsGenerator()
+    results_generator.load_reformat_CS50_dicts(controller.InputCS50Csv)
+    results_generator.get_student_results(controller.Slugs, controller.Tasks, controller.Choices)
+
+    results = results_generator.StudentResultsPreliminary
+
+    if controller.PlagiarismCheck:
+        downloader = Downloader()
+
+        downloader.submission_downloader(controller.Slugs, results)
+
+        if controller.DistributionURL:
+            for url in controller.DistributionURL:
+                downloader.distro_downloader(url)
+
+            plagiarism_checker = Plagiarism()
+            results = plagiarism_checker.run_plagiarism_check(controller.Slugs, controller.Sentimental, controller.Archive, results)
+
+            results_generator.update_student_results_plagiarism(results)
+    else:
+        results_generator.update_results_no_plagiarism()
+
+    controller.write_grade_table(results_generator.StudentResultsFinal,
+                                 results_generator.PassingStudents, results_generator.PlagiarisingStudents)
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
+
