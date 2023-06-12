de816cea6b5a1ab2a58d7a8c3f2c745313c89b5c
Sebastian Dunzer <sebastian.dunzer@gmail.com>
2023-06-12 11:10:51 +0200

    Fixed pylint of downloader.py

--- psgrade/downloader.py
+++ psgrade/downloader.py
@@ -1,21 +1,31 @@
+"""
+This module downloads submissions from GitHub via access_token
+"""
 import os
-import requests
 import zipfile
 import getpass
 import shutil
-
 from pathlib import Path
 from threading import Thread
 
-
-
+import requests
 
 
 class Downloader:
+    """
+    Class with functionality to download student submissions from GitHub
+    """
     def __init__(self):
+        """Constructor getting the Access-Token as a starred string"""
         self.__token = Downloader.__get_token_from_user()
 
-    def submission_downloader(self, slugs, results):
+    def submission_downloader(self, slugs:str, results:dict) -> None:
+        """
+        Parallelized download of submissions in different threads
+        :param slugs: slugs for the url to be downloaded
+        :param results: dictionary extracted from Submit50-csvs
+        :return: None
+        """
         if not Path("submissions").is_dir():
             os.mkdir("submissions")
         for slug in slugs:
@@ -23,25 +33,33 @@ class Downloader:
                 os.mkdir(f"submissions/{slug.replace('/', '_')}")
             threads = []
             for student in results:
-                t = Thread(target=self.__download_submission, args=(results, slug, student))
-                threads.append(t)
+                thread = Thread(target=self.__download_submission, args=(results, slug, student))
+                threads.append(thread)
             for thread in threads:
                 thread.start()
             for thread in threads:
                 thread.join()
 
     def __download_submission(self, result_dict, slug, student):
+        """
+        Downloads the submission
+        :param result_dict: Dict that contains all student submissions
+        :param slug: The slug for the task to be downloaded
+        :param student: The student whose submission should be downloaded
+        :return: None
+        """
         if slug not in result_dict[student]:
             return
         student_path = f"submissions/{slug.replace('/', '_')}/{student}.zip"
         if os.path.exists(''.join(student_path.split('.')[:-1])):
             return
-        url = f"https://github.com/me50/{student}/archive/{result_dict[student][slug].split('/')[-1]}.zip"
+        url = f"https://github.com/me50/{student}" \
+              f"/archive/{result_dict[student][slug].split('/')[-1]}.zip"
         headers = {'Authorization': f'token {self.__token}'}
-        r = requests.get(url, stream=True, headers=headers)
-        with open(student_path, "wb") as fd:
-            for chunk in r.iter_content(chunk_size=1024):
-                fd.write(chunk)
+        request = requests.get(url, stream=True, headers=headers, timeout=1000000)
+        with open(student_path, "wb") as student_file:
+            for chunk in request.iter_content(chunk_size=1024):
+                student_file.write(chunk)
         with zipfile.ZipFile(student_path) as zip_file:
             zip_file.extractall(''.join(student_path.split('.')[:-1]))
         os.remove(student_path)
@@ -49,22 +67,34 @@ class Downloader:
 
     @staticmethod
     def distro_downloader(url):
-        r = requests.get(url, stream=True)
-        distro_path = f"submissions/distribution.zip"
-        with open(distro_path, "wb") as fd:
-            for chunk in r.iter_content(chunk_size=1024):
-                fd.write(chunk)
+        """
+        Downloads the distribution code
+        :param url: from this url
+        :return: None
+        """
+        request = requests.get(url, stream=True, timeout=100000)
+        distro_path = "submissions/distribution.zip"
+        with open(distro_path, "wb") as distribution_file:
+            for chunk in request.iter_content(chunk_size=1024):
+                distribution_file.write(chunk)
         with zipfile.ZipFile(distro_path) as zip_file:
             zip_file.extractall(''.join(distro_path.split('.')[:-1]))
         os.remove(os.path.join(os.getcwd(), "submissions", "distribution.zip"))
 
     @staticmethod
-    def clean_dir(student_path):
+    def clean_dir(student_path:str) -> None:
+        """
+        Deletes all files from a path
+        :param student_path: Deletes the student file_path
+        :return: None
+        """
         student_path = os.path.join(os.getcwd(), student_path)
         for directory in os.listdir(student_path):
             if os.path.isdir(os.path.join(student_path, directory)):
-                for f in os.listdir(os.path.join(student_path, directory)):
-                    shutil.move(os.path.join(student_path, directory, f), os.path.join(student_path, f))
+                for file in os.listdir(os.path.join(student_path, directory)):
+                    full_path = os.path.join(student_path, directory, file)
+                    parent_of_full_path = os.path.join(student_path, file)
+                    shutil.move(full_path, parent_of_full_path)
             if os.path.isdir(os.path.join(student_path, directory)):
                 os.rmdir(os.path.join(student_path, directory))
             else:
@@ -72,6 +102,8 @@ class Downloader:
 
     @staticmethod
     def __get_token_from_user():
+        """
+        Gets an access token from a user
+        :return: returns the entered access token
+        """
         return getpass.getpass("Enter GitHub Access Token")
-
-
