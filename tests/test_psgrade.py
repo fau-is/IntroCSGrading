3f419994539acc70d1a9d7273265e3b159af636c
jfilleboeck <j.filleboeck@gmail.com>
2023-08-08 11:39:36 +0200

    Refactored version of the psgrade test

--- tests/test_psgrade.py
+++ tests/test_psgrade.py
@@ -4,115 +4,84 @@ from psgrade.psgrade import main
 
 
 class TestPsGrade(unittest.TestCase):
+    """
+    Unit tests for the psgrade module.
+    The tests check, that all functions which are called in psgarde, are called once
+    and with the correct arguments.
+    """
 
-    # def setup
-    @patch('psgrade.psgrade.Downloader')
-    @patch('psgrade.psgrade.Controller')
-    @patch('psgrade.psgrade.parse_args')
-    def test_call_parse_args(
-        self,
-        mock_parse_args,
-        mock_controller,
-        mock_downloader
-    ):
-        mock_args = MagicMock()
-        mock_parse_args.return_value = mock_args
+    def setUp(self):
+        """
+        Set up mock objects for testing
+        """
+        self.mock_parse_args = patch('psgrade.psgrade.parse_args').start()
+        self.mock_controller = patch('psgrade.psgrade.Controller').start()
+        self.mock_downloader = patch('psgrade.psgrade.Downloader').start()
+        self.mock_results_generator = patch('psgrade.psgrade.ResultsGenerator').start()
+        self.mock_plagiarism = patch('psgrade.psgrade.Plagiarism').start()
+
+        self.mock_args = MagicMock()
+        self.mock_parse_args.return_value = self.mock_args
+
+    def tearDown(self):
+        """
+        Clean up resources after each test. This method is called after each test method,
+        so that the mocks do not interfere with other tests.
+        """
+        patch.stopall()
+
+    def test_call_parse_args(self):
+        """
+        Test that the parse_args function is called once.
+        """
         main()
-        mock_parse_args.assert_called_once()
+        self.mock_parse_args.assert_called_once()
 
-    @patch('psgrade.psgrade.Downloader')
-    @patch('psgrade.psgrade.Controller')
-    @patch('psgrade.psgrade.parse_args')
-    def test_call_set_commandline_args(
-        self,
-        mock_parse_args,
-        mock_controller,
-        mock_downloader
-    ):
-        mock_args = MagicMock()
-        mock_parse_args.return_value = mock_args
+    def test_call_set_commandline_args(self):
+        """
+        Test that set_commandline_args method is called with the correct arguments.
+        """
         main()
-        mock_controller.return_value.set_commandline_args.assert_called_once_with(mock_args)
+        self.mock_controller.return_value.set_commandline_args.assert_called_once_with(self.mock_args)
 
-    @patch('psgrade.psgrade.Downloader')
-    @patch('psgrade.psgrade.Controller')
-    @patch('psgrade.psgrade.ResultsGenerator')
-    @patch('psgrade.psgrade.parse_args')
-    def test_call_results_generator_methods(
-        self,
-        mock_parse_args,
-        mock_results_generator,
-        mock_controller,
-        mock_downloader
-    ):
-        mock_args = MagicMock()
-        mock_parse_args.return_value = mock_args
+    def test_call_results_generator_methods(self):
+        """
+        Test that the methods of the ResultsGenerator class are called.
+        """
         main()
-        mock_results_generator.return_value.load_reformat_cs50_dicts.assert_called_once()
-        mock_results_generator.return_value.get_student_results.assert_called_once()
+        self.mock_results_generator.return_value.load_reformat_cs50_dicts.assert_called_once()
+        self.mock_results_generator.return_value.get_student_results.assert_called_once()
 
-    @patch('psgrade.psgrade.parse_args')
-    @patch('psgrade.psgrade.Downloader')
-    @patch('psgrade.psgrade.Plagiarism')
-    @patch('psgrade.psgrade.ResultsGenerator')
-    @patch('psgrade.psgrade.Controller')
-    def test_plagiarism_check(
-        self,
-        mock_controller,
-        mock_results_generator,
-        mock_plagiarism,
-        mock_downloader,
-        mock_parse_args
-    ):
-        mock_args = MagicMock()
-        mock_parse_args.return_value = mock_args
-        mock_controller.return_value.plagiarism_check = True
-        mock_controller.return_value.distribution_url = ['http://example.com']
+    def test_plagiarism_check(self):
+        """
+        Test the behavior when plagiarism_check is set to True.
+        """
+        self.mock_controller.return_value.plagiarism_check = True
+        self.mock_controller.return_value.distribution_url = ['http://example.com']
 
         main()
 
-        # Assert that the correct methods are called when plagiarism_check is True
-        mock_downloader.return_value.submission_downloader.assert_called()
-        mock_downloader.return_value.distro_downloader.assert_called()
-        mock_plagiarism.return_value.run_plagiarism_check.assert_called()
-        mock_results_generator.return_value.update_student_results_plagiarism.assert_called()
+        self.mock_downloader.return_value.submission_downloader.assert_called()
+        self.mock_downloader.return_value.distro_downloader.assert_called()
+        self.mock_plagiarism.return_value.run_plagiarism_check.assert_called()
+        self.mock_results_generator.return_value.update_student_results_plagiarism.assert_called()
 
-    @patch('psgrade.psgrade.parse_args')
-    @patch('psgrade.psgrade.Downloader')
-    @patch('psgrade.psgrade.Plagiarism')
-    @patch('psgrade.psgrade.ResultsGenerator')
-    @patch('psgrade.psgrade.Controller')
-    def test_without_plagiarism_check(
-        self,
-        mock_controller,
-        mock_results_generator,
-        mock_plagiarism,
-        mock_downloader,
-        mock_parse_args
-    ):
-        mock_args = MagicMock()
-        mock_parse_args.return_value = mock_args
-        mock_controller.return_value.plagiarism_check = False
+    def test_without_plagiarism_check(self):
+        """
+        Test the behavior when plagiarism_check is set to False.
+        """
+        self.mock_controller.return_value.plagiarism_check = False
 
         main()
 
-        # Assert that the correct method is called when plagiarism_check is False
-        mock_results_generator.return_value.update_results_no_plagiarism.assert_called()
+        self.mock_results_generator.return_value.update_results_no_plagiarism.assert_called()
 
-    @patch('psgrade.psgrade.Downloader')
-    @patch('psgrade.psgrade.Controller')
-    @patch('psgrade.psgrade.parse_args')
-    def test_call_write_grade_table(
-        self,
-        mock_parse_args,
-        mock_controller,
-        mock_downloader
-    ):
-        mock_args = MagicMock()
-        mock_parse_args.return_value = mock_args
+    def test_call_write_grade_table(self):
+        """
+        Test that the write_grade_table method is called once.
+        """
         main()
-        mock_controller.return_value.write_grade_table.assert_called_once()
-
+        self.mock_controller.return_value.write_grade_table.assert_called_once()
 
 if __name__ == '__main__':
     unittest.main()
